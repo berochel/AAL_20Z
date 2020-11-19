Fixing my dumbass idea about reversing the less-than-equal sign

dict.py
@@ -18,22 +18,21 @@ alphabet = {
     'n': 14,
     'o': 15,
     'p': 16,
-    'q': 17,
-    'r': 18,
-    's': 19,
-    't': 20,
-    'u': 21,
-    'w': 22,
-    'y': 23,
-    'z': 24,
-    'ą': 25,
-    'ś': 26,
-    'ć': 27,
-    'ź': 28,
-    'ż': 29,
-    'ó': 30,
-    'ł': 31,
-    'ę': 32
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'w': 21,
+    'y': 22,
+    'z': 23,
+    'ą': 24,
+    'ś': 25,
+    'ć': 26,
+    'ź': 27,
+    'ż': 28,
+    'ó': 29,
+    'ł': 30,
+    'ę': 31
 }
 
 
@@ -44,29 +43,26 @@ class Dict:
         output_list = []
         output = ""
         for x in range(f):
-            i = 0
-            j = 0
-            rand_number = random.randint(1, 31)  # ę not included in generating new words
-            # print(rand_number)
-            # print(list(alphabet.keys())[list(alphabet.values()).index(rand_number)])
+            rand_number = random.randint(1, 30)  # ę not included in generating new words
             j = rand_number
             output += list(alphabet.keys())[list(alphabet.values()).index(j)]
-            while j < 33:
-
-                rand_number = random.randint(1, 33 + len(output))
-                if rand_number > 33:
-                    rand_number = 33
+            while j < 32:
+                rand_number = random.randint(1, 32)
+                if rand_number > 32:
+                    rand_number = 32
                 rand_number2 = random.randint(0, 100000) / 100000
-                if self.probability_table[j][rand_number] < rand_number2:
-                    if rand_number < 33:
+                if self.probability_table[j][rand_number] > rand_number2:
+                    if rand_number < 32:
                         output += list(alphabet.keys())[list(alphabet.values()).index(rand_number)]
                     j = rand_number
+
             output_list += [output]
             output = ""
+            j = 0
         return output_list
 
     def __init__(self):
-        self.probability_table = [[0.0 for i in range(34)] for j in range(33)]
+        self.probability_table = [[0.0 for i in range(33)] for j in range(32)]
         with open('365_dni.txt', 'r', encoding='utf-8') as file:
 
             # reading each line
@@ -82,25 +78,18 @@ class Dict:
                         for key, value in alphabet.items():
                             if letter == key:
                                 i = value
-                                # print(letter)
-                                # print(str(i) + " " + str(j))
-                                # print(np.matrix(self.probability_table))
                                 self.probability_table[i][0] += 1
                                 self.probability_table[j][i] += 1
                                 j = i
-                        # print(letter)
-                    i = 33  # code for the "end of word" sign
+
+                    i = 32  # code for the "end of word" sign
                     self.probability_table[j][i] += 1
-                    # print(str(i) + " " + str(j))
-                    # print(word)
-        # print('\n'.join([''.join(['{:8.3f}'.format(item) for item in row])
-        #                 for row in self.probability_table]))
 
         for keyrow, valuerow in alphabet.items():
             if self.probability_table[valuerow][0] != 0:
                 for keycol, valuecol in alphabet.items():
                     self.probability_table[valuerow][valuecol] /= self.probability_table[valuerow][0]
-                self.probability_table[valuerow][33] /= self.probability_table[valuerow][0]
+                self.probability_table[valuerow][32] /= self.probability_table[valuerow][0]
 
-        print('\n'.join([''.join(['{:8.3f}'.format(item) for item in row])
+        print('\n'.join([''.join(['{:10.3f}'.format(item) for item in row])
                          for row in self.probability_table]))